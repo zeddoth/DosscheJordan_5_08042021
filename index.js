dfa236c9c2d32fb37f9f75cb5ac53c813e29d8f1
zeddoth <oblidark@gmail.com>
2021-04-08 16:00:55 +0200

    ajout des fichiers / appelle de l'API et premieres lignes CSS

---
+++ index.js
@@ -0,0 +1,38 @@
+const results = document.getElementById("results");
+
+let cameras;
+
+//API REQUEST
+
+const fetchCameras = async() => {
+    cameras = await fetch("https://ab-p5-api.herokuapp.com/api/cameras").then(res => res.json());
+};
+
+const showCameras = async() => {
+    await fetchCameras();
+    
+    results.innerHTML = (
+
+        cameras
+            .filter(camera => camera.name.toLowerCase())
+            .map(camera => (
+
+                `
+                <li class="camera-item"> 
+                    <div class="camera-infos">
+                        <h3 class="camera-name"><strong>${camera.name}</strong></h3>
+                        <h2 class="camera-price"><strong>${numberWithSpace(camera.price)} €</strong></h2>
+                    </div>
+                    <img class="camera-img" src="${camera.imageUrl}" />
+                </li>
+
+                `
+            )).join("")
+    );
+};
+
+showCameras();
+
+function numberWithSpace(x){
+	return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+}
